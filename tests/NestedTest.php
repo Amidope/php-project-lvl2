1eb35baa31976fbe6b1a5275044690ff858bb3f3
amidope <breakingdd@gmail.com>
2023-03-02 12:53:49 +0300

    create nested test

--- tests/NestedTest.php
+++ tests/NestedTest.php
@@ -11,5 +11,56 @@ class NestedTest extends TestCase
     protected $jsonPath2;
     protected $pathToYaml1;
     protected $pathToYaml2;
-    protected $expected1;
+    protected $expectedString;
+
+    public function setUp(): void
+    {
+        $this->expectedString = <<<RES
+        {
+            common: {
+            + follow: false
+                setting1: Value 1
+            - setting2: 200
+            - setting3: true
+            + setting3: null
+            + setting4: blah blah
+            + setting5: {
+                    key5: value5
+                }
+                setting6: {
+                    doge: {
+                    - wow:
+                    + wow: so much
+                    }
+                    key: value
+                + ops: vops
+                }
+            }
+            group1: {
+            - baz: bas
+            + baz: bars
+                foo: bar
+            - nest: {
+                    key: value
+                }
+            + nest: str
+            }
+        - group2: {
+                abc: 12345
+                deep: {
+                    id: 45
+                }
+            }
+        + group3: {
+                deep: {
+                    id: {
+                        number: 45
+                    }
+                }
+                fee: 100500
+            }
+        }
+        RES;
+    }
 }
+
