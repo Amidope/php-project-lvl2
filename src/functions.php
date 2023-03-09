4d8949f64031eb907c389082c62b628c3d687e7b
amidope <breakingdd@gmail.com>
2023-03-09 15:56:19 +0300

    add builder

--- src/functions.php
+++ src/functions.php
@@ -3,6 +3,7 @@
 namespace Php\Project\Lvl2\Functions;
 
 use function Functional\sort;
+use function Functional\map;
 use function Php\Project\Lvl2\parsers\parseJson;
 use function Php\Project\Lvl2\parsers\parseYaml;
 
@@ -44,44 +45,65 @@ function treeSort($tree)
 	//usort($tree, fn($innerArr1, $innerArr2) => strcmp($innerArr1, $innerArr2));
 	return $tree;
 }
-
+// make arr2 [] by default
 function buildTree($arr1, $arr2 = null)
 {
 	$result = [];
 
 	// если $arr2 == null
 	if (!$arr2) {
-		return array_map(function($key, $value) {
-			if (is_array($value)) {
-				return ['sign' => null, "key" => $key, 'children' => buildTree($value)]; 
+		return array_map(function($key, $val) {
+			if (is_array($val)) {
+				return ['sign' => null, "key" => $key, 'value' => buildTree($val)]; 
 			}
-			return ['sign' => null, "key" => $key, 'children' => $value];
+			return ['sign' => null, "key" => $key, 'value' => $val];
 		}, array_keys($arr1), array_values($arr1));
 	}
 
-	foreach ($arr1 as $key => $arr1Value) {
-		$arr2Value = $arr2[$key] ?? null;
+	// $arr1UniqueByKeys = array_diff_key($arr1, $arr2); 
+	// $tree1 = map($arr1UniqueByKeys, fn($val, $key, $col) => ['sign' => '-', "key" => $key, 'value' => is_array($val) ? buildTree($val) : $val]);
 
-		// если ключ есть в $arr2
-		if ($arr2Value) {
-			if (is_array($arr1Value)) {
-				$result[] = ['sign' => null, "key" => $key, 'children' => buildTree($arr1Value, $arr2Value)];
-			} elseif ($arr1Value == $arr2Value) {
-				$result[] = ['sign' => null, "key" => $key, 'children' => $arr1Value];
-			} else {
-				$result[] = ['sign' => "-", "key" => $key, 'children' => $arr1Value];
-				$result[] = ['sign' => "+", "key" => $key, 'children' => $arr2Value];
-			}
+	// $arr2UniqueByKeys = array_diff_key($arr2, $arr1); 
+	// $tree2 = map($arr2UniqueByKeys, fn($val, $key, $col) => ['sign' => '+', "key" => $key, 'value' => is_array($val) ? buildTree($val) : $val]);
+	print_r($arr1);
+	$matchedKeys = array_intersect(array_keys($arr1), array_keys($arr2));
+
+	$matchedTree = array_reduce($matchedKeys, function($acc, $key) use ($arr1,$arr2) {
+		
+		if (is_array($arr1[$key])) {
+			$acc[] = ['sign' => null, "key" => $key, 'value' => buildTree($arr1[$key], $arr2[$key])];
 		} else {
-			$result[] = ['sign' => "-", "key" => $key, 'children' => is_array($arr1Value) ? buildTree($arr1Value) : $arr1Value];
+			$acc[] = ['sign' => "-", "key" => $key, 'value' => $arr1[$key]];
+			$acc[] = ['sign' => "+", "key" => $key, 'value' => $arr2[$key]];
 		}
-	}
+		//print_r($acc);
+		return $acc;
+	}, []);
 
-	$secondArrDiff = array_diff_key($arr2, $arr1);
-	foreach ($secondArrDiff as $key => $value) {
-		$result[] = ['sign' => "+", "key" => $key, 'children' => is_array($arr1Value) ? buildTree($arr1Value) : $arr1Value];
-	}
-    return treeSort($result);
+
+	// foreach ($arr1 as $key => $arr1Value) {
+	// 	$arr2Value = $arr2[$key] ?? null;
+
+	// 	// если ключ есть в $arr2
+	// 	if ($arr2Value) {
+	// 		if (is_array($arr1Value)) {
+	// 			$result[] = ['sign' => null, "key" => $key, 'value' => buildTree($arr1Value, $arr2Value)];
+	// 		} elseif ($arr1Value == $arr2Value) {
+	// 			$result[] = ['sign' => null, "key" => $key, 'value' => $arr1Value];
+	// 		} else {
+	// 			$result[] = ['sign' => "-", "key" => $key, 'value' => $arr1Value];
+	// 			$result[] = ['sign' => "+", "key" => $key, 'value' => $arr2Value];
+	// 		}
+	// 	} else {
+	// 		$result[] = ['sign' => "-", "key" => $key, 'value' => is_array($arr1Value) ? buildTree($arr1Value) : $arr1Value];
+	// 	}
+	// }
+
+	// $secondArrDiff = array_diff_key($arr2, $arr1);
+	// foreach ($secondArrDiff as $key => $value) {
+	// 	$result[] = ['sign' => "+", "key" => $key, 'value' => is_array($arr1Value) ? buildTree($arr1Value) : $arr1Value];
+	// }
+    // return treeSort($result);
 }
 
 function gendiff($pathToFile1, $pathToFile2)
@@ -91,5 +113,5 @@ function gendiff($pathToFile1, $pathToFile2)
     // $stringifiedArr1 = stringifyBool($file1);
     // $stringifiedArr2 = stringifyBool($file2);
     $res = buildTree($file1,$file2);
-    var_dump($res);   
+    //var_dump($res);   
 }
