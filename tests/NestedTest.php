27a597b3b69c8370a83ac8aaa6f611e8d6687c9b
amidope <breakingdd@gmail.com>
2023-03-15 10:56:18 +0300

    add yml fixtures

--- tests/NestedTest.php
+++ tests/NestedTest.php
@@ -24,44 +24,44 @@ class NestedTest extends TestCase
         $this->jsonPath1 = $this->getFixtureFullPath('nestedFile1.json');
         $this->jsonPath2 = $this->getFixtureFullPath('nestedFile2.json');
         
-
-        $this->expectedString = <<<RES
+        
+        $this->expectedString = <<<RES        
         {
             common: {
-            + follow: false
+              + follow: false
                 setting1: Value 1
-            - setting2: 200
-            - setting3: true
-            + setting3: null
-            + setting4: blah blah
-            + setting5: {
+              - setting2: 200
+              - setting3: true
+              + setting3: null
+              + setting4: blah blah
+              + setting5: {
                     key5: value5
                 }
                 setting6: {
                     doge: {
-                    - wow:
-                    + wow: so much
+                      - wow:
+                      + wow: so much
                     }
                     key: value
-                + ops: vops
+                  + ops: vops
                 }
             }
             group1: {
-            - baz: bas
-            + baz: bars
+              - baz: bas
+              + baz: bars
                 foo: bar
-            - nest: {
+              - nest: {
                     key: value
                 }
-            + nest: str
+              + nest: str
             }
-        - group2: {
+          - group2: {
                 abc: 12345
                 deep: {
                     id: 45
                 }
             }
-        + group3: {
+          + group3: {
                 deep: {
                     id: {
                         number: 45
