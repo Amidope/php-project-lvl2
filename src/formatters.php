46036a7a4725bfe67e4868c0d0462de009d289bc
amidope <breakingdd@gmail.com>
2023-03-19 16:21:54 +0300

    add stylish

---
+++ src/formatters.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Php\Project\Lvl2\Formatters;
+
+use function Docopt\dump as dumperDocopt;
+function stylish($node, $indent = 0, $spacesCount = 4)
+{
+	
+	//
+	$lineIndent = str_repeat(" ", $indent + $spacesCount - 2);
+
+	$result = array_reduce($node, function ($acc, $item) use ($lineIndent, $indent, $spacesCount) {
+		$value = $item['value'];
+		$value = is_null($value) ? "null" : $value;
+		dump($item['key']);
+		dump('==');
+		dump($lineIndent);
+		$valueString = is_array($value) ? stylish($value, $indent + $spacesCount) : var_export($value, true);
+		
+		$acc .= "{$lineIndent}{$item['sign']} {$item['key']}: {$valueString}\n";
+		return $acc;
+	}, "");
+	//dump($indentString);
+	$closBracketIndent = $lineIndent = str_repeat(" ", $indent);
+	return "{\n{$result}{$closBracketIndent}}";
+}
