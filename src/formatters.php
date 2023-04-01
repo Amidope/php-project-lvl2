47ed09e751ffe4b0f3f93d357d53b7030c794818
amidope <breakingdd@gmail.com>
2023-04-01 15:04:31 +0300

    add plain

--- src/formatters.php
+++ src/formatters.php
@@ -3,6 +3,12 @@
 namespace Php\Project\Lvl2\Formatters;
 
 use function Docopt\dump as dumperDocopt;
+use function Functional\reduce_left;
+use function Php\Project\Lvl2\Functions\findPair;
+use function Php\Project\Lvl2\Functions\markAsProcessed;
+use function Php\Project\Lvl2\Functions\stringifyPlain;
+use function Php\Project\Lvl2\Functions\reduceWithFor;
+
 
 function stylish($node, $indent = 0, $spacesCount = 4)
 {
@@ -16,6 +22,7 @@ function stylish($node, $indent = 0, $spacesCount = 4)
             $valueString = is_array($value)
                 ? stylish($value, $indent + $spacesCount)
                 : trim(var_export($value, true), "'");
+            
             $valueString = $valueString === "" ? $valueString : " {$valueString}";
             $acc .= "{$lineIndent}{$item['sign']} {$item['key']}:{$valueString}\n";
             return $acc;
@@ -24,4 +31,49 @@ function stylish($node, $indent = 0, $spacesCount = 4)
     );
     $closBracketIndent = $lineIndent = str_repeat(" ", $indent);
     return "{\n{$result}{$closBracketIndent}}";
+}
+
+
+function plain($tree)
+{
+    $iter = function($tree, $path = '') use (&$iter)
+    {
+        return reduceWithFor(
+            $tree,
+            function($item, $index, &$col, $acc) use ($path, $iter) {
+                $key = $item['key'];
+                $val = $item['value'];
+                $sign = $item['sign'];
+                if (array_key_exists('processed', $item)) {
+                    return $acc;
+                }
+                markAsProcessed($col, $index);                
+                $path = $path ? "{$path}.{$key}" : $key;
+
+                if ($sign === ' ') {
+                    if (is_array($val)) {
+                        $formatted = $iter($val, $path);
+                        return [...$acc, ...$formatted];
+                    }
+                    return $acc;
+                }
+
+                $pair = findPair($col, $key);
+                if ($pair) {
+                    markAsProcessed($col, array_key_first($pair));
+                    $pair = array_pop($pair);
+                }
+                $string = stringifyPlain($path, $item, $pair);
+                $acc[] = $string;
+                return $acc;
+            },
+            []
+        );
+        //dump($tree);
+    };
+    $arr = $iter($tree);
+
+    $res = implode("\n", $arr);
+    // dump($res);
+return $res;
 }
