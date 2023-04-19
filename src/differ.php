4bd5e2ba8b80ae9c7ead5203eb67fbfb90553998
amidope <breakingdd@gmail.com>
2023-04-19 15:41:01 +0300

    add filepath validation

--- src/differ.php
+++ src/differ.php
@@ -5,11 +5,14 @@ namespace Differ\Differ;
 use function Differ\Functions\getDataByExtension;
 use function Differ\Functions\checkForEmptyness;
 use function Differ\Functions\isValidFormat;
-use function Differ\Builder\BuildTree;
+use function Differ\Builder\buildTree;
 use function Differ\Functions\getDiffByFormat;
 
-function genDiff(string $pathToFile1, string $pathToFile2, $format = 'stylish'): string
+function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string|bool
 {
+    if (!(file_exists($pathToFile1) && file_exists($pathToFile2))) {
+        return false;
+    }
     $arr1 = getDataByExtension($pathToFile1);
     $arr2 = getDataByExtension($pathToFile2);
     $message = checkForEmptyness($arr1, $arr2);
