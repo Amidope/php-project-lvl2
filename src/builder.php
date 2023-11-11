1a1682d969e65468b5f7aaebe0b7ca0fa175d936
Amidope <breakingdd@gmail.com>
2023-11-11 19:33:39 +0300

    refactoring

--- src/builder.php
+++ src/builder.php
@@ -2,71 +2,92 @@
 
 namespace Differ\Builder;
 
-use function Differ\Functions\buildNode;
 use function Differ\Functions\treeSort;
+use function Functional\map;
 
-function buildTree(array $arr1, array $arr2 = [])
+function buildTree(mixed $arr1, mixed $arr2 = []): array
 {
-    $result = [];
     if (!$arr2) {
-        $result = array_map(
-            function ($key, $val) {
-                $value = is_array($val) ? buildTree($val) : $val;
-                return buildNode($key, $value);
-            },
+        $result = ['isList' => false, 'value' => $arr1];
+        if (!is_array($arr1)) {
+            return $result;
+        }
+        if (array_is_list($arr1)) {
+            return array_merge($result, ['isList' => true]);
+        }
+        $value = array_map(
+            fn ($key, $val) => buildUnchanged($key, $val),
             array_keys($arr1),
             array_values($arr1)
         );
-        return treeSort($result);
+        return array_merge($result, ['value' => $value]);
     }
 
-    $arr1UniqueByKeys = array_diff_key($arr1, $arr2);
-    $tree1 = array_map(
-        function ($key, $val) {
-            return buildNode($key, is_array($val) ? buildTree($val) : $val, "-");
-        },
-        array_keys($arr1UniqueByKeys),
-        array_values($arr1UniqueByKeys)
-    );
+    $addedPairs = array_diff_key($arr2, $arr1);
+    $addedPairsTree = map($addedPairs, fn ($value, $key, $col) => buildAdded($key, $value));
 
-    $arr2UniqueByKeys = array_diff_key($arr2, $arr1);
-    $tree2 = array_map(
-        function ($key, $val) {
-            return buildNode($key, is_array($val) ? buildTree($val) : $val, "+");
-        },
-        array_keys($arr2UniqueByKeys),
-        array_values($arr2UniqueByKeys)
-    );
+    $deletedPairs = array_diff_key($arr1, $arr2);
+    $deletedPairsTree = map($deletedPairs, fn ($value, $key, $col) => buildDeleted($key, $value));
 
     $matchedKeys = array_intersect(array_keys($arr1), array_keys($arr2));
-    $matchedTree = array_reduce(
-        $matchedKeys,
-        function ($acc, $key) use ($arr1, $arr2) {
-            $val1 = $arr1[$key];
-            $val2 = $arr2[$key];
-            if (is_array($val1) && is_array($val2)) {
-                $children = buildTree($val1, $val2);
-                $acc[] = buildNode($key, $children);
-                return $acc;
-            } elseif (!is_array($val1) && !is_array($val2)) {
-                if ($val1 === $val2) {
-                    $acc[] = buildNode($key, $val1);
-                } else {
-                    $acc[] = buildNode($key, $val1, "-");
-                    $acc[] = buildNode($key, $val2, "+");
-                }
-                return $acc;
-            }
-
-            $value1 = is_array($val1) ? buildTree($val1) : $val1;
-            $value2 = is_array($val2) ? buildTree($val2) : $val2;
-            $acc[] = buildNode($key, $value1, "-");
-            $acc[] = buildNode($key, $value2, "+");
-            return $acc;
-        },
-        []
+    $matchedTree = array_map(function ($key) use ($arr1, $arr2) {
+        $val1 = $arr1[$key];
+        $val2 = $arr2[$key];
+        if ($val1 === $val2) {
+            return buildUnchanged($key, $val1);
+        }
+        if (isAssocArray($val1) && isAssocArray($val2)) {
+            return buildUpdated($key, $val1, $val2);
+        }
+        return buildChanged($key, $val1, $val2);
+    }, $matchedKeys);
+    return treeSort([...$deletedPairsTree, ...$addedPairsTree, ...$matchedTree]);
+}
+function buildAdded(string $key, mixed $value): array
+{
+    return array_merge(
+        ['type' => 'added', 'key' => $key],
+        buildTree($value)
     );
-    $result = [...$matchedTree, ...$tree1, ...$tree2];
-    $sorted = treeSort($result);
-    return $sorted;
+}
+;;
+function buildDeleted(string $key, mixed $value): array
+{
+    return array_merge(
+        ['type' => 'deleted', 'key' => $key],
+        buildTree($value)
+    );
+}
+function buildUnchanged(string $key, mixed $value): array
+{
+    return array_merge(
+        ['type' => 'unchanged', 'key' => $key],
+        buildTree($value)
+    );
+}
+function buildChanged(string $key, mixed $val1, mixed $val2): array
+{
+    return [
+        'type' => 'changed',
+        'key' => $key,
+        'oldValue' => buildTree($val1),
+        'newValue' => buildTree($val2)
+    ];
+}
+function buildUpdated(string $key, array $val1, array $val2): array
+{
+    return [
+        'type' => 'updated',
+        'key' => $key,
+        'value' => buildTree($val1, $val2)
+    ];
+}
+function isAssocArray($arr)
+{
+    if (is_array($arr)) {
+        if (!array_is_list($arr)) {
+            return true;
+        }
+    }
+    return false;
 }
